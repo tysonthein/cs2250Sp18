465734fd11d50d0baab7e18e1aa549781c699635
Tyson Thein <tysonthein@mail.weber.edu>
2018-01-23 10:14:46 -0700

    "these are practice for strings"

---
+++ module3/strings.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  strings.c
+ *
+ *    Description:  string practice
+ *
+ *        Version:  1.0
+ *        Created:  01/23/2018 10:09:07 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Tyson Thein (), tysonthein@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    char name[11];
+    printf("enter your name:\n");
+    scanf("%s", name);
+    printf("your name is: [%s]\n", name);
+    return 0;
+}
+// Function Definitions
+
+
