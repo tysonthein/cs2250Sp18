826dbe207dc2b6394e2da3761366b0d2175ce2e6
Tyson Thein <tysonthein@mail.weber.edu>
2018-01-30 10:14:40 -0700

    initial commit

---
+++ module4/multibranch.c
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  multibranch.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/30/2018 10:11:06 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Tyson Thein (), tysonthein@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int income = 30000;
+    if(income)
+    {
+        printf("income is %d\n", income);
+    }
+    else{
+        printf("no income\n");
+    }
+    return 0;
+}
+// Function Definitions
+
+
